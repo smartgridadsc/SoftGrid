ab9c530d74ba9c2c4d9027cc07e7837328d263fc
prageethmahendra <prageethmahendra@gmail.com>
2017-05-05 12:11:53 +0800

    Interceptor interface implementation

---
+++ SoftGrid/src/main/java/it/illinois/adsc/ema/interceptor/InterceptorFactory.java
@@ -0,0 +1,13 @@
+package it.illinois.adsc.ema.interceptor;
+
+/**
+ * Created by prageethmahendra on 5/5/2017.
+ */
+public class InterceptorFactory {
+    public static InterceptorListObject initInterceptors(){
+        // todo JAVA reflection code to instantiate interceptors
+        // todo wrap them inside InterceptorListObject ( custom linked list)
+        // todo return the root of linked list interceptor object list.
+        return null;
+    }
+}
